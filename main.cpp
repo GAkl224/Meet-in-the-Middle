8373c19151b21ca63523445cd9a26846df03ee88
GAkl224 <156771707+GAkl224@users.noreply.github.com>
2024-02-25 22:36:41 -0600

    update

--- main.cpp
+++ main.cpp
@@ -1,43 +1,78 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#include <sstream>
 #include <vector>
-#include <string>
-
+#include <algorithm>
 
 using namespace std;
-void readFile(){
-    string fileName;
-    ifstream file;
 
-    //user input
-    //user enters file name to recover
-    cout << "Enter file name: ";
-    getline(cin, fileName);
+double find_median(const vector<int>& data) {
 
-    file.open(fileName);
+    int n = data.size();
 
-    string number;
+    if (n % 2 == 0) {
 
-    getline(file, number);
+        int middle1 = data[n / 2 - 1];
+        int middle2 = data[n / 2];
 
-    //while (getline(file, number)) {
-      //  cout << line << endl;
-        //cout << 1;
-   // }
-    cout << 1;
-    while(!file.eof()){
-        cout << 1;
-                file >> number;
-            cout << number << ",";
-        cout << 2;
-            }
-            cout << 3;
-    file.close();
+        return (middle1 + middle2) / 2.0;
+
+    } else {
+
+        return data[n / 2];
+    }
 }
-int main()
+
+int readFile()
 {
-    
+    // Get the filename from the user
+    string file_path;
+
+    cout << "Enter the file name: ";
+    cin >> file_path;
+
+    ifstream file(file_path);
+
+    if (!file.is_open()) {
+
+        cout << "Unable to open file '" << file_path << "'.";
+        return 1;
+    }
+
+    vector<int> data;
+    string line;
+
+    //read file integers
+    while (getline(file, line)) {
+
+        stringstream ss(line);
+        int number;
+
+        while (ss >> number) {
+
+            data.push_back(number);
+
+            if (ss.peek() == ',') {
+                ss.ignore();
+            }
+        }
+    }
+
+    file.close();
+
+
+    //call function to find median
+    double median = find_median(data);
+
+    // output
+    cout << "The median of the data is: " << median;
+
+    return 0;
+
+}
+
+int main() {
     readFile();
+
     return 0;
 }
