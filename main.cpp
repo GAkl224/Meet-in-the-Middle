f6f699a67fba5d0553c9e0adfc8c2d3a4b1aa173
GAkl224 <156771707+GAkl224@users.noreply.github.com>
2024-04-27 10:35:51 -0500

    Update - (Three Credits) Median Plus Bubble Sort
    code has been updated to include profile two additions

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -13,28 +12,43 @@ double find_median(const vector<int>& data) {
     if (n % 2 == 0) {
 
         int middle1 = data[n / 2 - 1];
+
         int middle2 = data[n / 2];
 
         return (middle1 + middle2) / 2.0;
 
     } else {
-
         return data[n / 2];
     }
 }
 
-int readFile()
-{
-    // Get the filename from the user
-    string file_path;
+void bubbleSort(vector<int>& data) {
 
+    int n = data.size();
+
+    for (int i = 0; i < n - 1; ++i) {
+
+        for (int j = 0; j < n - i - 1; ++j) {
+
+            if (data[j] > data[j + 1]) {
+
+                // Swap elements
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int readFile() {
+    string file_path;
     cout << "Enter the file name: ";
     cin >> file_path;
 
     ifstream file(file_path);
 
     if (!file.is_open()) {
-
         cout << "Unable to open file '" << file_path << "'.";
         return 1;
     }
@@ -42,12 +56,12 @@ int readFile()
     vector<int> data;
     string line;
 
-    //read file integers
+    // Read file integers
     while (getline(file, line)) {
 
         stringstream ss(line);
-        int number;
 
+        int number;
         while (ss >> number) {
 
             data.push_back(number);
@@ -57,22 +71,21 @@ int readFile()
             }
         }
     }
-
     file.close();
 
+    // sort using bubble sort
+    bubbleSort(data);
 
-    //call function to find median
+    // Call function to find median
     double median = find_median(data);
 
-    // output
+    // Output
     cout << "The median of the data is: " << median;
 
     return 0;
-
 }
 
 int main() {
     readFile();
-
     return 0;
 }
